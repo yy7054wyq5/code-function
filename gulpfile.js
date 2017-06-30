28cec10259a0d1c72ba8602bdbd0e1516f6ae982
wuyang <yang_wu@loongjoy.com>
2017-06-30 15:10:45 +0800

    去除eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
-import gulp from 'gulp';
+var gulp = require('gulp');
+var less = require('gulp-less');
+var sourcemaps = require('gulp-sourcemaps');
+var ts = require('gulp-typescript');
+var tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('default', function () {
-	console.log(1);
+	gulp.src(['src/**/*.ts'])
+		.pipe(tsProject())
+		.js.pipe(gulp.dest('build'));
 });
