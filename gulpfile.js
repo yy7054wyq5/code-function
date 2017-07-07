faa6e1fe17a3e01c0fcf979078839b9443909f90
wuyang <yang_wu@loongjoy.com>
2017-07-07 14:51:09 +0800

    整理结构

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,16 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+
+gulp.task('less', function () {
+  return gulp.src('src/**/*.less')
+    .pipe(less())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('dist'))
+});
