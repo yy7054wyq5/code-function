7f4a63af4a0541b22e59f9df2f6c38dd8af5e326
wuyang <yang_wu@loongjoy.com>
2017-09-26 09:58:31 +0800

    ```

--- src/ellipsis/ellipsis.js
+++ src/ellipsis/ellipsis.js
@@ -1,21 +1,20 @@
   /**
-  *多文字省略号处理
-  */
-  function ellipsis (txt,obj) {
+   *多文字省略号处理
+   */
+  function ellipsis(txt, obj) {
     //txt 显示的最大字数
     //obj jQuery对象
     var $ellipsisWord = obj;
-    if(obj==undefined){
+    if (obj == undefined) {
       //IE8 bug
-    }
-    else{
-      for (i = 0; i < $ellipsisWord.length; i++){
-            var text = $ellipsisWord[i];
-            str = text.innerHTML;
-            var textLeng = txt;
-            if (str.length > textLeng) {
-                text.innerHTML = str.substring(0, textLeng) + "...";
-            }
+    } else {
+      for (i = 0; i < $ellipsisWord.length; i++) {
+        var text = $ellipsisWord[i];
+        str = text.innerHTML;
+        var textLeng = txt;
+        if (str.length > textLeng) {
+          text.innerHTML = str.substring(0, textLeng) + "...";
+        }
       }
     }
   }
